4f3f8d6faf7bed7bca5884f4c70b48df3a38bf36
JeisonCanas <jeison.canas@pragma.com.co>
2023-05-29 12:55:46 -0500

    Inicio test unit Create Owner

---
+++ 1. User/User.Test/User.Test/Config/ApplicationDbContextInMemory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using User.DataBase;
+
+namespace User.Test.Config
+{
+    public static class ApplicationDbContextInMemory
+    {
+        public static UserDBContext Get()
+        {
+            var options = new DbContextOptionsBuilder<UserDBContext>()
+                .UseInMemoryDatabase(databaseName: $"User.Db")
+                .Options;
+
+            return new UserDBContext(options);
+        }
+    }
+}
